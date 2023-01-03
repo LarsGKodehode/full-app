d34a50770e767946cf2dc26d918d26b9ba9f029b
LarsG <lars.ga.solheim@gmail.com>
2023-01-03 08:21:10 +0100

    added some more details to planet data

--- src/RQ/UniverseBodies/query.tsx
+++ src/RQ/UniverseBodies/query.tsx
@@ -57,13 +57,21 @@ function PlanetCard(props: StellarBody) {
   return (
     <div className="flex max-w-xl px-2 py-2 my-4 border-2 border-solid rounded-lg border-slate-800 bg-slate-600/80">
       <div className="w-1/2 text-sm">
-        <p>Planet Name: <br /> {props.englishName}</p>
-        <p>Discovered by: <br /> {props.discoveredBy}</p>
-        <p>Discovery date: <br /> {props.discoveryDate}</p>
+        <p>
+          Planet Name: <br /> {props.englishName}
+        </p>
+        <p>
+          Discovered by: <br /> {props.discoveredBy}
+        </p>
+        <p>
+          Discovery date: <br /> {props.discoveryDate}
+        </p>
       </div>
 
       <div className="w-1/2 text-xs flex flex-col items-end">
         <p>Gravity: {props.gravity}</p>
+        <p>Equatorial Radius: {props.equaRadius}</p>
+        <p>Inclination: {props.inclination}</p>
         <MoonList moons={props.moons} />
       </div>
     </div>
