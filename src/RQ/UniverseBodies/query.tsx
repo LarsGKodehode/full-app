3865c9b7374bf5e973bbafe8c04547c0d1723f2b
LarsG <lars.ga.solheim@gmail.com>
2023-01-03 07:05:14 +0100

    added a simple accordion

--- src/RQ/UniverseBodies/query.tsx
+++ src/RQ/UniverseBodies/query.tsx
@@ -1,4 +1,5 @@
 // Libraries
+import { useState } from 'react';
 import { useQuery } from 'react-query';
 
 const URL_ENDPOINT = 'https://api.le-systeme-solaire.net/rest/bodies/';
@@ -7,12 +8,6 @@ const QUERY = '?filter[]=bodyType,eq,Planet';
 interface QueryResponse {
   bodies: StellarBody[];
 }
-
-type StellarBody = {
-  englishName: string;
-  discoveredBy: string;
-  discoveryDate: string;
-};
 
 function UniverseBodies() {
   const { isLoading, error, data } = useQuery(
@@ -43,24 +38,70 @@ function UniverseBodies() {
   );
 }
 
+
+type StellarBody = {
+  englishName: string;
+  discoveredBy: string;
+  discoveryDate: string;
+  gravity: number,
+  equaRadius: number,
+  inclination: number,
+  moons: StellarBodyMoon[] | undefined
+};
+
+type StellarBodyMoon = {
+  moon: string,
+  rel: string
+}
+
 function PlanetCard(props: StellarBody) {
   return (
-    <div className="flex px-2 py-2 my-4 border-2 border-solid rounded-lg border-slate-800 bg-slate-600/60">
+    <div className="flex max-w-xl px-2 py-2 my-4 border-2 border-solid rounded-lg border-slate-800 bg-slate-600/80">
       <div className="w-1/2 text-sm">
         <p>
           Planet Name: <br /> {props.englishName}
         </p>
+
         <p>
           Discovered by: <br /> {props.discoveredBy}
         </p>
+
         <p>
           Discovery date: <br /> {props.discoveryDate}
         </p>
       </div>
 
-      <div className="w-1/2 bg-yellow-300"></div>
+      <div className="w-1/2 text-xs text-right">
+        <p>
+          Gravity: {props.gravity}
+        </p>
+
+        <MoonList moons={props.moons} />
+      </div>
     </div>
   );
+}
+
+function MoonList({ moons }: { moons: StellarBodyMoon[] | undefined }) {
+  const [isHovering, setIsHovering] = useState(false)
+
+  return (
+    <div className='relative'
+      onMouseEnter={() => setIsHovering(true)}
+      onMouseLeave={() => setIsHovering(false)}
+    >
+      <p>
+        Moons: {moons ? moons.length : 0}
+      </p>
+
+      {
+        isHovering && moons &&
+        <ul className='absolute top-0 p-2 -right-20 rounded-xl bg-slate-600'>
+          {moons.map((moon, index) => <li key={index}>{moon.moon}</li>)}
+        </ul>
+      }
+    </div>
+  )
 }
 
 export default UniverseBodies;
